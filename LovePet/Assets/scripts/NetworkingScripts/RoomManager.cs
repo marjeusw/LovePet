888838f3da62d1776838856053c4d25c48f9fbf0
marje <marje.harms@web.de>
2022-08-04 19:02:21 +0200

    enemy in resources folder
    - add resources folder
    - add enemy blobb
    - add room manager

---
+++ LovePet/Assets/scripts/NetworkingScripts/RoomManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomManager : MonoBehaviour
+{
+    public static RoomManager Instance { set; get; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        //register to delegate
+        SceneManager.sceneLoaded += OnSceneLoad;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoad;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoad;
+    }
+
+    private void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        Vector3 spawningPos = new Vector3(Random.Range(-3f, 3f), 2.0f, Random.Range(-3f, 3f));
+
+        if (PhotonNetwork.InRoom)
+        {
+            //Instanciates by name (careful with spelling)
+            PhotonNetwork.Instantiate("blob", spawningPos, Quaternion.identity); //maybe only 1 for now to see if it works
+        }
+        else
+        {
+            Instantiate(Resources.Load("blob"), spawningPos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
