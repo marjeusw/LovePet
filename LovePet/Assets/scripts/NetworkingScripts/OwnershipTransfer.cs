b23af1c75a3671be9513b43dc447f5c6cceccac1
marje <marje.harms@web.de>
2022-08-04 19:17:59 +0200

    ownershiptranfer first test
    - add script

---
+++ LovePet/Assets/scripts/NetworkingScripts/OwnershipTransfer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+
+public class OwnershipTransfer : MonoBehaviour
+{
+    public void TransferOwnership(PhotonView photonView)
+    {
+        photonView.RequestOwnership();
+    }
+
+}
